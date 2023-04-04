33bf96592aa24ee6aeb4a6cbda800a9507277be4
WooHCode <gusdn526@naver.com>
2023-04-04 20:45:21 +0900

    initial commit

---
+++ src/main/java/teamproject/smokinglocation/SmokinglocationApplication.java
@@ -0,0 +1,13 @@
+package teamproject.smokinglocation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmokinglocationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmokinglocationApplication.class, args);
+	}
+
+}
