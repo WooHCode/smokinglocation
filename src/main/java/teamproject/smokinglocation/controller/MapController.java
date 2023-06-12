56c9ef396f8093f3ab121be077e7e6e38deb1215
hwhw.c <gusdn526@gmail.com>
2023-06-12 20:22:24 +0900

    feature SMOK-30 자주 사용하는 script구문 js파일로 추출하여 import하여 사용, 모든 지역구에 현재 위치를 가져오는 메서드 적용. localStorage에 위도경도 데이터를 저장하여 데이터를 갱신하지 않는 한 현재 위치 좌표 모든 지도에 출력

--- src/main/java/teamproject/smokinglocation/controller/MapController.java
+++ src/main/java/teamproject/smokinglocation/controller/MapController.java
@@ -5,22 +5,16 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import teamproject.smokinglocation.dto.*;
-import teamproject.smokinglocation.entity.Gangnam;
 import teamproject.smokinglocation.entity.TotalData;
 import teamproject.smokinglocation.service.DataResponseService;
-import teamproject.smokinglocation.service.DataService;
-import teamproject.smokinglocation.service.LatlngService;
+
 import teamproject.smokinglocation.service.LocationNearbyService;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -34,11 +28,17 @@ public class MapController {
 
     private final DataResponseService responseService;
     private final LocationNearbyService nearbyService;
+    private static String myLatitude = "";
+    private static String myLongitude = "";
 
 
     @GetMapping("/map")
     public String showMap(Model model) {
         List<TotalData> totalData = responseService.getTotalData();
+        if (!myLatitude.equals("") && !myLongitude.equals("")) {
+            model.addAttribute("myLatitude", myLatitude);
+            model.addAttribute("myLongitude",myLongitude);
+        }
         log.info("===========totalDataLoading===========");
         log.info("totalData = {}", totalData);
         log.info("===========totalDataLoadingFinish============");
@@ -56,6 +56,10 @@ public class MapController {
     @GetMapping("/map/nearby")
     @ResponseBody
     public List<NearbyLocationDto> getNearbyLocation(String latitude, String longitude) {
+        myLatitude = latitude;
+        myLongitude = longitude;
+        log.info("myLatitude = {}", myLatitude);
+        log.info("myLongitude = {}", myLongitude);
         List<TotalData> totalData = responseService.getTotalData();
         List<NearbyLocationDto> nearbyDtos = totalData.stream()
                 .map(data -> {
@@ -75,63 +79,143 @@ public class MapController {
         model.addAttribute("naverMapClientId", naverMapClientId);
 
         if (gu.equals("강남")) {
+            if (!myLatitude.equals("") && !myLongitude.equals("") && myLatitude != null && myLongitude != null) {
+                model.addAttribute("myLatitude", myLatitude);
+                model.addAttribute("myLongitude",myLongitude);
+            }
             model.addAttribute("facilities", responseService.getGangnamData());
             return "gangnam";
         } else if (gu.equals("강북")) {
+            if (!myLatitude.equals("") && !myLongitude.equals("") && myLatitude != null && myLongitude != null) {
+                model.addAttribute("myLatitude", myLatitude);
+                model.addAttribute("myLongitude",myLongitude);
+            }
             model.addAttribute("facilities", responseService.getGangbukData());
             return "gangbuk";
         } else if (gu.equals("강서")) {
+            if (!myLatitude.equals("") && !myLongitude.equals("") && myLatitude != null && myLongitude != null) {
+                model.addAttribute("myLatitude", myLatitude);
+                model.addAttribute("myLongitude",myLongitude);
+            }
             model.addAttribute("facilities", responseService.getGangseoData());
             return "gangseo";
         } else if (gu.equals("관악")) {
+            if (!myLatitude.equals("") && !myLongitude.equals("") && myLatitude != null && myLongitude != null) {
+                model.addAttribute("myLatitude", myLatitude);
+                model.addAttribute("myLongitude",myLongitude);
+            }
             model.addAttribute("facilities", responseService.getGwanakData());
             return "gwanak";
         } else if (gu.equals("광진")) {
+            if (!myLatitude.equals("") && !myLongitude.equals("") && myLatitude != null && myLongitude != null) {
+                model.addAttribute("myLatitude", myLatitude);
+                model.addAttribute("myLongitude",myLongitude);
+            }
             model.addAttribute("facilities", responseService.getGwangjinData());
             return "gwangjin";
         } else if (gu.equals("구로")) {
+            if (!myLatitude.equals("") && !myLongitude.equals("") && myLatitude != null && myLongitude != null) {
+                model.addAttribute("myLatitude", myLatitude);
+                model.addAttribute("myLongitude",myLongitude);
+            }
             model.addAttribute("facilities", responseService.getGuroData());
             return "guro";
         } else if (gu.equals("노원")) {
+            if (!myLatitude.equals("") && !myLongitude.equals("") && myLatitude != null && myLongitude != null) {
+                model.addAttribute("myLatitude", myLatitude);
+                model.addAttribute("myLongitude",myLongitude);
+            }
             model.addAttribute("facilities", responseService.getNowonData());
             return "nowon";
         } else if (gu.equals("동대문")) {
+            if (!myLatitude.equals("") && !myLongitude.equals("") && myLatitude != null && myLongitude != null) {
+                model.addAttribute("myLatitude", myLatitude);
+                model.addAttribute("myLongitude",myLongitude);
+            }
             model.addAttribute("facilities", responseService.getDongdaemunData());
             return "dongdaemun";
         } else if (gu.equals("동작")) {
+            if (!myLatitude.equals("") && !myLongitude.equals("") && myLatitude != null && myLongitude != null) {
+                model.addAttribute("myLatitude", myLatitude);
+                model.addAttribute("myLongitude",myLongitude);
+            }
             model.addAttribute("facilities", responseService.getDongjakData());
             return "dongjak";
         } else if (gu.equals("서대문")) {
+            if (!myLatitude.equals("") && !myLongitude.equals("") && myLatitude != null && myLongitude != null) {
+                model.addAttribute("myLatitude", myLatitude);
+                model.addAttribute("myLongitude",myLongitude);
+            }
             model.addAttribute("facilities", responseService.getSeodaemunData());
             return "seodaemun";
         } else if (gu.equals("서초")) {
+            if (!myLatitude.equals("") && !myLongitude.equals("") && myLatitude != null && myLongitude != null) {
+                model.addAttribute("myLatitude", myLatitude);
+                model.addAttribute("myLongitude",myLongitude);
+            }
             model.addAttribute("facilities", responseService.getSeochoData());
             return "seocho";
         } else if (gu.equals("성동")) {
+            if (!myLatitude.equals("") && !myLongitude.equals("") && myLatitude != null && myLongitude != null) {
+                model.addAttribute("myLatitude", myLatitude);
+                model.addAttribute("myLongitude",myLongitude);
+            }
             model.addAttribute("facilities", responseService.getSeongdongData());
             return "seongdong";
         } else if (gu.equals("성북")) {
+            if (!myLatitude.equals("") && !myLongitude.equals("") && myLatitude != null && myLongitude != null) {
+                model.addAttribute("myLatitude", myLatitude);
+                model.addAttribute("myLongitude",myLongitude);
+            }
             model.addAttribute("facilities", responseService.getSeongbukData());
             return "seongbuk";
         } else if (gu.equals("송파")) {
+            if (!myLatitude.equals("") && !myLongitude.equals("") && myLatitude != null && myLongitude != null) {
+                model.addAttribute("myLatitude", myLatitude);
+                model.addAttribute("myLongitude",myLongitude);
+            }
             model.addAttribute("facilities", responseService.getSongpaData());
             return "songpa";
         } else if (gu.equals("양천")) {
+            if (!myLatitude.equals("") && !myLongitude.equals("") && myLatitude != null && myLongitude != null) {
+                model.addAttribute("myLatitude", myLatitude);
+                model.addAttribute("myLongitude",myLongitude);
+            }
             model.addAttribute("facilities", responseService.getYangcheonData());
             return "yangcheon";
         } else if (gu.equals("영등포")) {
+            if (!myLatitude.equals("") && !myLongitude.equals("") && myLatitude != null && myLongitude != null) {
+                model.addAttribute("myLatitude", myLatitude);
+                model.addAttribute("myLongitude",myLongitude);
+            }
             model.addAttribute("facilities", responseService.getYeongdeungpoData());
             return "yeongdeungpo";
         } else if (gu.equals("용산")) {
+            if (!myLatitude.equals("") && !myLongitude.equals("") && myLatitude != null && myLongitude != null) {
+                model.addAttribute("myLatitude", myLatitude);
+                model.addAttribute("myLongitude",myLongitude);
+            }
             model.addAttribute("facilities", responseService.getYongsanData());
             return "yongsan";
         } else if (gu.equals("종로")) {
+            if (!myLatitude.equals("") && !myLongitude.equals("") && myLatitude != null && myLongitude != null) {
+                model.addAttribute("myLatitude", myLatitude);
+                model.addAttribute("myLongitude",myLongitude);
+            }
             model.addAttribute("facilities", responseService.getJongnoData());
             return "jongno";
         } else if (gu.equals("중구")) {
+            if (!myLatitude.equals("") && !myLongitude.equals("") && myLatitude != null && myLongitude != null) {
+                model.addAttribute("myLatitude", myLatitude);
+                model.addAttribute("myLongitude",myLongitude);
+            }
             model.addAttribute("facilities", responseService.getJungguData());
             return "junggu";
         } else if (gu.equals("중랑")) {
+            if (!myLatitude.equals("") && !myLongitude.equals("") && myLatitude != null && myLongitude != null) {
+                model.addAttribute("myLatitude", myLatitude);
+                model.addAttribute("myLongitude",myLongitude);
+            }
             model.addAttribute("facilities", responseService.getJungnangData());
             return "jungnang";
         }
