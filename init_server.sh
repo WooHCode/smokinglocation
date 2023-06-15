567cc4f748511c7e39c4e8634b12b15943d9b171
hwhw.c <gusdn526@gmail.com>
2023-06-16 01:28:03 +0900

    feature develop jenkins integration test4

---
+++ init_server.sh
@@ -0,0 +1,10 @@
+echo "> 기존 pid 확인"
+        CURRENT_PID=$(ps -ef | grep java | grep smokinglocation | grep -v nohup | awk '{print $2}')echo "$CURRENT_PID"
+        if [ -z ${CURRENT_PID} ] ;then
+        echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
+        else
+        echo "> sudo kill -9 $CURRENT_PID"
+        sudo kill -9 $CURRENT_PID
+        sleep 10
+        fi
+        echo "> smokinglocation 배포"
