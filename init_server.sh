5f032346fadaebca68f2f63144bdac05eb9ce4a8
hwhw.c <gusdn526@gmail.com>
2023-06-21 21:04:58 +0900

    feature develop ec2서버에 sh 파일 작동 확인

---
+++ init_server.sh
@@ -0,0 +1,25 @@
+REPOSITORY=build/libs
+
+echo "> cd $REPOSITORY"
+cd $REPOSITORY
+
+echo "> Finding the currently running app's PID"
+
+CURRENT_PID=$(pgrep -f smoking)
+
+echo "$CURRENT_PID"
+
+if [ -z $CURRENT_PID ]; then
+    echo "> No running app."
+else
+    echo "> kill -9 $CURRENT_PID"
+    kill -9 $CURRENT_PID
+    sleep 3
+fi
+
+echo "> Deploying the new app"
+
+echo "> nohup java -jar smokinglocation-0.0.1-SNAPSHOT.jar 2>1 &"
+nohup java -jar smokinglocation-0.0.1-SNAPSHOT.jar 2>1 &
+
+echo "> finish"
