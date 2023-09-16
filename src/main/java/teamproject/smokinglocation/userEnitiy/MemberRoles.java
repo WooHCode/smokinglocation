039479cc7854c858433ac06700403bfbb0d282ca
Hunesu Kim <kewcxea@hanmail.net>
2023-09-16 18:53:45 +0900

    오류 해결

--- src/main/java/teamproject/smokinglocation/userEnitiy/MemberRoles.java
+++ src/main/java/teamproject/smokinglocation/userEnitiy/MemberRoles.java
@@ -17,7 +17,7 @@ public class MemberRoles implements Serializable{
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
-    
+
     @Column(nullable = false)
     private String roles;
 }
