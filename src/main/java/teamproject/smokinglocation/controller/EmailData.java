0276f2514e0256b061041561cdfc5a48e2e7150b
chaehyunwoo <it.hwhwc@contentlap.com>
2024-01-15 18:53:27 +0900

    메인화면 디자인변경, 메일 시스템 정비중

--- src/main/java/teamproject/smokinglocation/controller/EmailData.java
+++ src/main/java/teamproject/smokinglocation/controller/EmailData.java
@@ -4,8 +4,8 @@ import lombok.Getter;
 
 @Getter
 public enum EmailData {
-    header("[서울시흡연구역]"),
-	answer("님이 작성하신 문의 답변이 달렸습니다."),
+    header("[서울시흡연구역] "),
+	answer("님이 작성하신 문의가 성공적으로 접수되었습니다."),
 	template1("/email/templateMail1");
     private final String emailString;
 
